77bcaf0c3740df1ff5da038542000e598c68a392
eirikwha <eirikwha@stud.ntnu.no>
2018-11-11 16:42:22 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/assignment3_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment3_server.dir/server.c.o"
+  "assignment3_server.pdb"
+  "assignment3_server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assignment3_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
